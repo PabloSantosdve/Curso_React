ada2d579f95ecdd7370cce442dd6004d0ed003c2
PabloSantosdve <148356304+PabloSantosdve@users.noreply.github.com>
2024-07-23 15:51:18 -0300

    .

--- meu_projeto/src/App.jsx
+++ meu_projeto/src/App.jsx
@@ -1,11 +1,17 @@
 import './App.css'
-
+import SayMyName from"./components/SayMyName"
+import Pessoa from './components/Pessoa'
 function App() {
-
 
   return (
     <>
-     
+    <SayMyName nome="Pablão" />
+    <SayMyName nome= "Joao"  />
+    <Pessoa  
+    nome="Pablo"
+    idade = "21"
+    profissao = "Programador"
+    foto = "https://via.placeholder.com/150" />
     </>
   )
 }
---
+++ meu_projeto/src/components/Frase.jsx
@@ -0,0 +1,11 @@
+function Frase(){
+
+	return(
+		<div>
+			<h2>Este é um compontente com uma frase</h2>
+			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae est illo dolorum. Eos, quisquam vel. Officia tempore quasi placeat, iusto eligendi, enim deleniti veritatis ipsa cumque voluptate, dicta beatae. Quam?</p>
+		</div>
+	)
+}
+
+export default Frase
