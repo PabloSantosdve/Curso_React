3cc6587c9f89f44302ad8ff31eb21b0bd3afc57e
PabloSantosdve <148356304+PabloSantosdve@users.noreply.github.com>
2024-07-24 15:13:53 -0300

    Feito mais duas aulas

--- meu_projeto/src/App.jsx
+++ meu_projeto/src/App.jsx
@@ -1,10 +1,15 @@
 import './App.css'
 import SayMyName from"./components/SayMyName"
 import Pessoa from './components/Pessoa'
+import Frase from './components/Frase'
+import List from './components/List'
+
+
 function App() {
 
   return (
     <>
+    <h1>Testando CSS</h1>
     <SayMyName nome="Pablão" />
     <SayMyName nome= "Joao"  />
     <Pessoa  
@@ -12,6 +17,11 @@ function App() {
     idade = "21"
     profissao = "Programador"
     foto = "https://via.placeholder.com/150" />
+
+    <Frase/>
+    <Frase/>
+    <List/>
+
     </>
   )
 }
--- meu_projeto/src/components/Frase.jsx
+++ meu_projeto/src/components/Frase.jsx
@@ -1,9 +1,11 @@
+import styles from './Frase.module.css'
+
 function Frase(){
 
 	return(
-		<div>
-			<h2>Este é um compontente com uma frase</h2>
-			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae est illo dolorum. Eos, quisquam vel. Officia tempore quasi placeat, iusto eligendi, enim deleniti veritatis ipsa cumque voluptate, dicta beatae. Quam?</p>
+		<div className={styles.fraseContainer}>
+			
+			<p className={styles.fraseContent}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae est illo dolorum. Eos, quisquam vel. Officia tempore quasi placeat, iusto eligendi, enim deleniti veritatis ipsa cumque voluptate, dicta beatae. Quam?</p>
 		</div>
 	)
 }
